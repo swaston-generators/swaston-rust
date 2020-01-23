4693e15a72d7cb97e2568db50665fcc32aaef11c
Wirtos <Wirtos.new@gmail.com>
2020-01-23 20:30:08 +0200

    Update to follow main code requirements

--- src/swaston_generator.rs
+++ src/swaston_generator.rs
@@ -31,31 +31,32 @@ pub fn generate_swaston(s: &str) -> String {
         return s.to_string();
     }
     let l_spaced_word = utf8_join(&s);
-    let r_spaced_word = utf8_reverse(&utf8_join(s));
+    let r_spaced_word = utf8_reverse(&l_spaced_word);
     let l_word = utf8_reverse(&s);
+    let r_word = s.to_string();
     let center = format!("{0}{1}\n",
                          l_spaced_word,
                          utf8_substr(&r_spaced_word, 1, utf8_distance(&r_spaced_word))
     );
     let tab_pre = " ".repeat(utf8_distance(&r_spaced_word) - 2);
-    let tab_post = format!("{0}{1}", tab_pre, " ");
-    let r_vec = utf8_to_vector(s);
+    let tab_post = tab_pre.clone() + " ";
+    let r_vec = utf8_to_vector(&r_word);
     let l_vec = utf8_to_vector(&l_word);
-    let size = l_vec.len();
+    let length = l_vec.len();
 
-    let mut upper  = String::new();
+    let mut upper = String::new();
     let mut lower = String::new();
 
-    for c in 0..size {
+    for c in 0..length {
         if c == 0 { // case first row
             upper.push_str(&format!("{0}{1}{2}\n", l_vec[c], tab_pre, l_spaced_word));
-        } else if c + 1 != size {
+        } else if c + 1 != length {
             upper.push_str(&format!("{0}{1}{2}{3}\n", l_vec[c], tab_pre, r_vec[c], tab_post));
         }
     }
 
-    for c in 1..size { // lower side
-        if c == size - 1 {  // case last row
+    for c in 1..length { // lower side
+        if c == length - 1 {  // case last row
             lower.push_str(&format!("{0}{1}{2}\n", r_spaced_word, tab_pre, r_vec[c]));
         } else {
             lower.push_str(&format!("{0}{1}{2}{3}\n", tab_post, l_vec[c], tab_pre, r_vec[c]));
